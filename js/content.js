f365cebadb15255577547494ee83c2c23c933ecd
Cenk Ebret <cenkebret@gmail.com>
2020-03-27 20:25:40 +0100

    master - scoped css selector

--- js/content.js
+++ js/content.js
@@ -4,23 +4,23 @@ var formatNr = input => {
 }
 
 $(document).ready(() => {
-    $('section').addClass('loading');
+    $('.covid-around section').addClass('loading');
     $.getJSON({url: "http://ip-api.com/json"}).done( ipInfo => {
         $.ajax({url: `https://corona.lmao.ninja/countries/${ipInfo.country.toLowerCase()}`}).done(
             data => {                
                 if (data) {
-                    $(".countryInfo").text(`Corona cases in ${ipInfo.country}`);
-                    $("#confirmed").text(`${formatNr(data.cases)}`);
-                    $("#confirmedtoday").text(`${formatNr(data.todayCases)}`);
-                    $("#recovered").text(`${formatNr(data.recovered)}`);
-                    $("#deaths").text(`${formatNr(data.deaths)}`);
-                    $("#deathstoday").text(`${formatNr(data.todayDeaths)}`);
-                    $("#deathrate").text(`${(Math.round((data.deaths / data.cases) * 10000) / 100) + " %"}`);
+                    $(".covid-around .countryInfo").text(`Corona cases in ${ipInfo.country}`);
+                    $(".covid-around #confirmed").text(`${formatNr(data.cases)}`);
+                    $(".covid-around #confirmedtoday").text(`${formatNr(data.todayCases)}`);
+                    $(".covid-around #recovered").text(`${formatNr(data.recovered)}`);
+                    $(".covid-around #deaths").text(`${formatNr(data.deaths)}`);
+                    $(".covid-around #deathstoday").text(`${formatNr(data.todayDeaths)}`);
+                    $(".covid-around #deathrate").text(`${(Math.round((data.deaths / data.cases) * 10000) / 100) + " %"}`);
                 } else {
-                    $("div").hide();
-                    $(".countryInfo").text(`Corona cases in ${ipInfo.country} could not be retrieved`);
+                    $(".covid-around div").hide();
+                    $(".covid-around .countryInfo").text(`Corona cases in ${ipInfo.country} could not be retrieved`);
                 }
-                $('section').removeClass('loading');
+                $('.covid-around section').removeClass('loading');
             } 
         );
     });    
